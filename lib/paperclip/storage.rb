f835de37e4b45afa084659dd37e758235fce5552
Bernat Foj Capell <bfoj@gnuine.com>
2011-11-03 08:59:57 +0100

    Using the path hash instead of rand to calculate the s3 host alias, faster and predictable (jhernandez)

--- lib/paperclip/storage.rb
+++ lib/paperclip/storage.rb
@@ -156,8 +156,9 @@ module Paperclip
           @bucket = @service.buckets.build(@bucket_name)
         end
         Paperclip.interpolates(:s3_alias_url) do |attachment, style|
-          host = (attachment.s3_host_alias =~ /%d/) ? attachment.s3_host_alias % (rand(4) + 1) : attachment.s3_host_alias.to_s
-          "#{attachment.s3_protocol}://#{host}/#{attachment.path(style).gsub(%r{^/}, "")}"
+          style_path = attachment.path(style)
+          host = (attachment.s3_host_alias =~ /%d/) ? attachment.s3_host_alias % ((style_path.hash % 4) + 1) : attachment.s3_host_alias.to_s
+          "#{attachment.s3_protocol}://#{host}/#{style_path.gsub(%r{^/}, "")}"
         end
         Paperclip.interpolates(:s3_path_url) do |attachment, style|
           "#{attachment.s3_protocol}://s3.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, "")}"
